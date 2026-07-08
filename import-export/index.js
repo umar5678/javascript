3fecc73e2a963d6ca8b098deb4610bc09bf5fc2e
umar <umarfarooq567800@gmail.com>
2026-07-08 17:08:15 +0500

    import and export

---
+++ import-export/index.js
@@ -0,0 +1,14 @@
+import  {saySalam}  from "./utils.js"
+
+
+let user = "Ismail"
+
+
+function greet (name) {
+    return saySalam(name)
+}
+
+
+const res = greet(user)
+
+console.log(res)
---
+++ import-export/utils.js
@@ -0,0 +1,7 @@
+export const saySalam = (user) => { // named export
+    return `Assalam-o-Alikum!, ${user}`
+}
+
+
+// export {saySalam} named export 
+// export default saySalam  // ------------ default export 
